f12fef5ce2ae032536ae23b7f09308a006200fff
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-18 16:09:13 +0100

    end js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let name= prompt("Dimmi il tuo nome");
+let surname= prompt ("Dimmi il tuo cognome");
+let favoriteColor= prompt("Dimmi il tuo colore preferito")
+
+document.getElementById("you").innerHTML= `Io so che ti chiami ${name} ${surname} e che il tuo colore preferito è il ${favoriteColor}`
